33a03fabe9dd47db6b092d8438cc28cb23aee1ae
rgulles <gulles.ronian@gmail.com>
2026-01-16 22:31:50 +0800

    add Setting class and refactor the main.py structure

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from tkinter import Tk
 
+from settings import Settings
+
 class Wordle:    
     def __init__(self, master):
         self.master = master
-        
+        self.settings = Settings()
+
         self.master.title('Wordle')
-        self.master.geometry('600x700')
+        self.master.geometry(f'{self.settings.screen_width}x{self.settings.screen_height}')
 
 if __name__ == "__main__":
     root = Tk()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+
+    def __init__(self):
+
+        # Screen
+        self.screen_width = 500
+        self.screen_height = 700
+
